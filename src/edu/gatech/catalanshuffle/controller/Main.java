fd4981fb976cb56dc403d7839a4292ea9857bd8f
Yiqi (Victor) Chen <yiqic1993@gmail.com>
2016-02-28 18:17:15 -0500

    add control for n value, tick speed and canvas type

--- src/edu/gatech/catalanshuffle/controller/Main.java
+++ src/edu/gatech/catalanshuffle/controller/Main.java
@@ -1,16 +1,25 @@
 package edu.gatech.catalanshuffle.controller;
 
+import javafx.animation.Animation.Status;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import edu.gatech.catalanshuffle.view.*;
@@ -18,7 +27,12 @@ import edu.gatech.catalanshuffle.view.*;
 public class Main extends Application {
 	
 	public static final int TICK_RATE = 400;
-	public static final int N = 20;
+	public static final int INITIAL_N = 15;
+	public static final String INITIAL_CANVAS = "Polygon Triangulation";
+	
+	BorderPane frame;
+	CatalanModelCanvas canvas;
+	Timeline timer;
 	 
     public static void main(String[] args) {
         launch(args);
@@ -28,29 +42,112 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Catalan Structure");
         Group root = new Group();
-        BorderPane frame = new BorderPane();
-//        CatalanModelCanvas canvas = new DyckPathCanvas(N, 900, 450);
-//        CatalanModelCanvas canvas = new DyckPathCollectionCanvas(N, 900, 450, 100);
-//        CatalanModelCanvas canvas = new DyckPathCouplingCanvas(N, 900, 450, false, true);
-        CatalanModelCanvas canvas = new PolygonTriangulationCanvas(N, 900, 450);
+        frame = new BorderPane();
+        setupCanvas(INITIAL_N, INITIAL_CANVAS);
+//        CatalanModelCanvas canvas = new DyckPathCanvas(N, 750, 450);
+//        CatalanModelCanvas canvas = new DyckPathCollectionCanvas(N, 750, 450, 100);
+//        CatalanModelCanvas canvas = new DyckPathCouplingCanvas(N, 750, 450, false, true);
+//        canvas = new PolygonTriangulationCanvas(INITIAL_N, 750, 450);
+//        
+//        timer = new Timeline(new KeyFrame(Duration.millis(TICK_RATE), new TickCanvas(canvas, 1)));
+//        timer.setCycleCount(Timeline.INDEFINITE);
+//        
+//        frame.setCenter(canvas);
+//        frame.setRight(getControlPanel(canvas));
+//        frame.setBottom(getVariablePanel(canvas));
+        root.getChildren().add(frame);
+        primaryStage.setScene(new Scene(root));
+        frame.setBottom(getVariablePanel());
         
-        Timeline timer = new Timeline(new KeyFrame(Duration.millis(TICK_RATE), new TickCanvas(canvas, 1)));
+        primaryStage.show();
+//        timer.play();
+    }
+    
+    public void setupCanvas(int n, String type) {
+    	if (timer != null) {
+    		timer.stop();
+    	}
+        canvas = CatalanModelCanvasFactory(n, type);
+        
+        timer = new Timeline(new KeyFrame(Duration.millis(TICK_RATE), new TickCanvas(canvas, 1)));
         timer.setCycleCount(Timeline.INDEFINITE);
         
         frame.setCenter(canvas);
-        frame.setBottom(getControlPanel(timer, canvas));
-        root.getChildren().add(frame);
-        primaryStage.setScene(new Scene(root));
+        frame.setRight(getControlPanel());
         
-        primaryStage.show();
         timer.play();
     }
     
-    public HBox getControlPanel(Timeline timer, CatalanModelCanvas canvas) {
-        HBox hbox = new HBox();
-        hbox.setPadding(new Insets(15, 12, 15, 12));
-        hbox.setSpacing(10);
-        hbox.setStyle("-fx-background-color: #336699;");
+    public HBox getVariablePanel() {
+    	HBox hbox = new HBox();
+    	hbox.setPadding(new Insets(15, 12, 15, 12));
+    	hbox.setSpacing(10);
+    	hbox.setStyle("-fx-background-color: #336699;");
+    	
+    	ComboBox<String> viewType = new ComboBox<>();
+    	viewType.getItems().addAll(
+        	"Polygon Triangulation",
+        	"Single Dyck Path",
+            "100 Dyck Paths",
+            "Dyck Path Coupling"
+        );
+    	viewType.setValue(viewType.getItems().get(0));
+    	
+    	Slider nSlider = new Slider(5, 50, INITIAL_N);
+    	Label nLabel = new Label("n: ");
+    	Label nValue = new Label(Integer.toString((int)nSlider.getValue()));
+    	nLabel.setFont(new Font("Arial", 15));
+    	nLabel.setTextFill(Color.WHITE);
+    	nValue.setFont(new Font("Arial", 15));
+    	nValue.setTextFill(Color.WHITE);
+    	nSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                Number old_val, Number new_val) {
+            	int n = (int)nSlider.getValue();
+            	nValue.setText(String.format("%02d", n));
+            	setupCanvas(n, viewType.getValue());
+            }
+        });
+    	viewType.valueProperty().addListener(new ChangeListener<String>() {
+            @Override 
+            public void changed(@SuppressWarnings("rawtypes") ObservableValue ov, String t, String t1) {         
+            	int n = (int)nSlider.getValue();
+            	setupCanvas(n, viewType.getValue()); 
+            }    
+        });
+    	
+    	Slider tickSlider = new Slider(50, 999, TICK_RATE);
+    	Label tickLabel = new Label("speed (millisecond): ");
+    	Label tickValue = new Label(Integer.toString((int)tickSlider.getValue()));
+    	tickLabel.setFont(new Font("Arial", 15));
+    	tickLabel.setTextFill(Color.WHITE);
+    	tickValue.setFont(new Font("Arial", 15));
+    	tickValue.setTextFill(Color.WHITE);
+    	tickSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                Number old_val, Number new_val) {
+            	boolean inAnimation = timer.getStatus() == Status.RUNNING;
+            	int rate = (int)tickSlider.getValue();
+            	timer.stop();
+            	timer.getKeyFrames().clear();
+            	timer.getKeyFrames().add(new KeyFrame(Duration.millis(rate), new TickCanvas(canvas, 1)));
+            	timer.setCycleCount(Timeline.INDEFINITE);
+            	tickValue.setText(String.format("%03d", rate));
+            	if (inAnimation) {
+            		timer.play();
+            	}
+            }
+        });
+    	
+    	hbox.getChildren().addAll(viewType, nLabel, nValue, nSlider, tickLabel, tickValue, tickSlider);
+    	return hbox;
+    }
+    
+    public VBox getControlPanel() {
+    	VBox vbox = new VBox();
+        vbox.setPadding(new Insets(15, 12, 15, 12));
+        vbox.setSpacing(10);
+        vbox.setStyle("-fx-background-color: #336699;");
 
         Button pause = new Button("Pause");
         pause.setPrefSize(100, 20);
@@ -80,8 +177,25 @@ public class Main extends Application {
             }
         });
         
-        hbox.getChildren().addAll(pause, play, tick, tick5, reset);
-        return hbox;
+        vbox.getChildren().addAll(pause, play, tick, tick5, reset);
+        return vbox;
+    }
+    
+    private CatalanModelCanvas CatalanModelCanvasFactory(int n, String type) {
+    	int width = 800;
+    	int height = 450;
+    	switch(type) {
+    	case "Polygon Triangulation": 
+    		return new PolygonTriangulationCanvas(n, width, height);
+    	case "Single Dyck Path": 
+    		return new DyckPathCanvas(n, width, height);
+    	case "100 Dyck Paths": 
+    		return new DyckPathCollectionCanvas(n, width, height, 100);
+    	case "Dyck Path Coupling": 
+    		return new DyckPathCouplingCanvas(n, width, height, false, true);
+    	default: 
+    		return null;
+    	}
     }
     
     private class TickCanvas implements EventHandler<ActionEvent> {
--- src/edu/gatech/catalanshuffle/test/TestMain.java
+++ src/edu/gatech/catalanshuffle/test/TestMain.java
@@ -1,38 +1,38 @@
 package edu.gatech.catalanshuffle.test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import edu.gatech.catalanshuffle.model.*;
 import edu.gatech.catalanshuffle.model.CatalanModel.DistanceMetric;
 
-public class Main {
+public class TestMain {
 
 	public static void main(String[] args) {
 		DyckPath model = new DyckPath(20);
-		double[][] p = new double[15][32];
+		double[][] p = new double[29][32];
+		int numExp = 1;
 //		double[] p2 = new double[32];
 //		double expectedNum = 5;
-		long trials = 1000;
+		long trials = 10000;
 		int stepSize = 8;
 		int n = 5;
 		for (int k = 0; k < p.length; k++) {
 			model = new DyckPath(n);
 			System.out.print(n);
-			for (int i = 0; i < 50; i++) {
+			for (int i = 0; i < numExp; i++) {
 				int itr = 0;
 				for (int j = 0; j < p[0].length; j++) {
-					List<double[][]> res = model.distributionExperiment(trials, itr);
+					List<double[][]> res = model.distributionExperiment(trials*n, itr);
 					double[] pvall = model.testUniformDistribution(res, false, DistanceMetric.AVERAGEDISTANCE);
 					p[k][j] += pvall[2];
 					itr += stepSize;
 				}
 			}
 			for (int j = 0; j < p[0].length; j++) {
-				System.out.print("\t" + (p[k][j]/50));
+				System.out.print("\t" + (p[k][j]/(numExp*trials*n)));
 			}
 			System.out.println();
-			n += 2;
+			n += 1;
 		}
 
 //		model = new DyckPath(14);
@@ -55,13 +55,15 @@ public class Main {
 //		}
 
 		int itr = 0;
+		n = 5;
 		for (int j = 0; j < p[0].length; j++) {
 			System.out.print(itr);
 			for (int i = 0; i < p.length; i++) {
 //				List<int[][]> res = model.distributionExperiment(expectedNum, itr);
-				System.out.print("\t" + (p[i][j]/50));
+				System.out.print("\t" + (p[i][j]/(numExp*trials*n)));
 			}
 			itr += stepSize;
+			n++;
 			System.out.println();
 		}
 	}
